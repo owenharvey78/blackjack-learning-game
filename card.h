bb618d8c92f7da52a42bce0ae82c25c691a8597e
Carter Elgie <carter.elgie@gmail.com>
2025-11-18 13:28:12 -0700

    Create Card class

---
+++ card.h
@@ -0,0 +1,15 @@
+#ifndef CARD_H
+#define CARD_H
+
+#include <QObject>
+
+class Card : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Card(QObject *parent = nullptr);
+
+signals:
+};
+
+#endif // CARD_H
