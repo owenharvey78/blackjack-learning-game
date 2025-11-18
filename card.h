ef0099fc2e5c869bfd1c6edbbcaea6db8dba5aab
Carter Elgie <carter.elgie@gmail.com>
2025-11-18 13:42:05 -0700

    Add declarations for Card class

--- card.h
+++ card.h
@@ -7,9 +7,52 @@ class Card : public QObject
 {
     Q_OBJECT
 public:
-    explicit Card(QObject *parent = nullptr);
+    /// @brief The possible suits for a playing card. "Cut" represents the cut card,
+    /// which tells the dealer when to shuffle the shoe.
+    enum class Suit {
+        Spades,
+        Hearts,
+        Clubs,
+        Diamonds,
+        Cut
+    };
 
-signals:
+    /// @brief The possible ranks for a playing card. "Cut" describes the cut card,
+    /// which tells the dealer when to shuffle the shoe.
+    enum class Rank {
+        Cut,
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    };
+
+    /// @brief Creates a new Card object.
+    /// @param rank The rank of the card.
+    /// @param suit The suit of the card.
+    /// @param parent The parent of this Card object.
+    explicit Card(Rank rank, Suit suit, QObject *parent = nullptr);
+
+    /// @brief Gets the value of this card according to the Hi-Lo card counting system.
+    /// @return 1 if this card's rank is 2-6, 0 if this card's rank is 7-9, or -1 if this
+    /// card's rank is jack-ace.
+    int getValue();
+
+private:
+    /// @brief The card's suit.
+    Suit suit;
+
+    /// @brief The card's rank.
+    Rank rank;
 };
 
 #endif // CARD_H
