18f3b5667f6c84f67e424f1ac64cb75a8582cc1f
Carter Elgie <carter.elgie@gmail.com>
2025-11-20 12:42:01 -0700

    Implement card constructor and methods

--- card.cpp
+++ card.cpp
@@ -1,5 +1,15 @@
 #include "card.h"
 
-Card::Card(QObject *parent)
-    : QObject{parent}
+Card::Card(Rank rank, Suit suit, QObject *parent)
+    : QObject{parent}, rank_(rank), suit_(suit)
 {}
+
+int Card::getValue() const {
+    if (static_cast<int>(rank_) >= 2 && static_cast<int>(rank_) <= 6) {
+        return 1;
+    }
+    else if (rank_ == Rank::Ace || static_cast<int>(rank_) >= 10) {
+        return -1;
+    }
+    return 0;
+}
