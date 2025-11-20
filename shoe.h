022f7e37a31d5f960f762140ef85341c1687b40d
Carter Elgie <carter.elgie@gmail.com>
2025-11-20 12:13:53 -0700

    Add QObject inheritance to class declaration

--- shoe.h
+++ shoe.h
@@ -3,8 +3,10 @@
 
 #include <QObject>
 
-class Shoe
+/// @brief A class representing the shoe that holds the cards to be drawn.
+class Shoe : public QObject
 {
+    Q_OBJECT
 public:
     Shoe();
 };
