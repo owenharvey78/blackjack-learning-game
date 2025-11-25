1028a6bd4d735ddaeff329a42d801e7cc0ddd70c
Carter Elgie <carter.elgie@gmail.com>
2025-11-25 12:19:46 -0700

    Start implementing betting ui

--- game_widget.h
+++ game_widget.h
@@ -2,7 +2,9 @@
 #define GAME_WIDGET_H
 
 #include <QWidget>
+#include <QVector>
 #include "blackjack_game.h"
+#include "card.h"
 
 namespace Ui {
 class GameWidget;
@@ -22,12 +24,26 @@ public:
     /// @brief Frees any resources associated with this GameWidget.
     ~GameWidget();
 
+public slots:
+    /// @brief Displays the card that was dealt to the dealer.
+    void dealerCardDealt(Card card);
+
 private:
     /// @brief The UI form associated with this widget.
     Ui::GameWidget* ui_;
 
     /// @brief The game currently being played.
     BlackjackGame game_;
+
+    /// @brief The cards the dealer currently has.
+    QVector<Card> dealerHand_;
+
+    /// @brief The cards the player currently has.
+    QVector<QVector<Card>> playerHands_;
+
+    /// @brief During the betting stage, this represents the bet the player will make
+    /// once they click the "start" button.
+    int betToPlace_;
 };
 
 #endif // GAME_WIDGET_H
